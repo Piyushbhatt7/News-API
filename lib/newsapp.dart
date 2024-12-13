4fc8078affeb59c61411e62c8ef31590bad07a43
vengersmoon <vengersmon003@gmail.com>
2024-12-13 14:32:38 +0530

    v

--- lib/newsapp.dart
+++ lib/newsapp.dart
@@ -1,1 +1,47 @@
-statefull
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_api/newsmodel.dart';
+
+class NewsApp extends StatefulWidget {
+  const NewsApp({super.key});
+
+  @override
+  State<NewsApp> createState() => _NewsAppState();
+}
+
+class _NewsAppState extends State<NewsApp> {
+
+  Future<NewsModel>fetchNews() async{
+     
+     final url = "https://newsapi.org/v2/everything?q=tesla&from=2024-11-13&sortBy=publishedAt&apiKey=e17e0929e5734ce38ae87013c2e894d8"
+
+     var response = await http.get(Uri.parse(url));
+
+     if(response.statusCode==200){
+        final result = jsonDecode(response.body);
+        return NewsModel.fromJson(result);
+     }
+
+     else{
+          return NewsModel();
+     }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("News App"),
+        centerTitle: true,
+      ),
+        
+        body: FutureBuilder(future: fetchNews(), builder: (context, snapshot){
+           
+           return ListView.builder(itemBuilder: (context, index){
+                  return
+           },);
+        }),
+    );
+  }
+}
